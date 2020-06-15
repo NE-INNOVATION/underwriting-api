99d7c0f2d34f3948e4c58d886696394eb9843e8d
Vikas Saxena <65643139+saxena-vikas@users.noreply.github.com>
2020-06-14 23:39:17 -0400

    updating config

--- server/authConfiguration.js
+++ server/authConfiguration.js
@@ -1,5 +1,5 @@
-var ISSUER = process.env.ISSUER || 'https://dev-521711.okta.com/oauth2/default';
-var CLIENT_ID = process.env.CLIENT_ID || '0oadgcu962ydolarc4x6';
+var ISSUER = process.env.ISSUER;
+var CLIENT_ID = process.env.CLIENT_ID;
 
 module.exports = {
   server: {
